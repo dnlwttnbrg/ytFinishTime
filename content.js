5e5fab3c1e61aab7225e7f8790b8f6477ad20633
Daniel Wittenburg <wittenburg.daniel@web.de>
2022-07-18 12:52:58 +0200

    bug fix

--- content.js
+++ content.js
@@ -8,23 +8,29 @@ tag.setAttribute("id", "finichedAtTime")
 var currentInterval = setInterval(function() {
     current = document.getElementsByClassName("ytp-time-current")[0] //get curent time of the video
     if(current != undefined){
+        
         duration = document.getElementsByClassName("ytp-time-duration")[0] //get duration of the video
         timeDisplay = document.getElementsByClassName("ytp-time-display")[0] //get time container to add new time to
         timeDisplay.appendChild(tag)
         observer.observe(current, {characterData: false, childList: true, attributes: false});
         clearInterval(currentInterval)
+        
     }
   }, 1000); // 1000 milliseconds (1 second)
 
 //observes changes in current time
 observer = new MutationObserver(function(mutationsList, observer) {
-    current = document.getElementsByClassName("ytp-time-current")[0] //get curent time of the video
-    
-    var speed = document.getElementsByClassName("video-stream html5-main-video")[0].playbackRate
+    //if(document.getElementsByClassName("ytp-live-badge")[0].disabled == true){
+    //    tag.innerHTML = ("asd") 
+    //}else{
+        current = document.getElementsByClassName("ytp-time-current")[0] //get curent time of the video
+        
+        var speed = document.getElementsByClassName("video-stream html5-main-video")[0].playbackRate
 
-    var seconds = (hmsToSeconds(duration.innerHTML)) - (hmsToSeconds(current.innerHTML))
-    var newTime = addSeconds(Math.floor(seconds / parseFloat(speed)))
-    tag.innerHTML = ("(" + newTime.toLocaleTimeString().substring(0,5) + ")")
+        var seconds = (hmsToSeconds(duration.innerHTML)) - (hmsToSeconds(current.innerHTML))
+        var newTime = addSeconds(Math.floor(seconds / parseFloat(speed)))
+        tag.innerHTML = ("(Finished at " + newTime.toLocaleTimeString().substring(0,5) + ")")
+    //}
 });
 
 //Converts hh:mm:ss to seconds
